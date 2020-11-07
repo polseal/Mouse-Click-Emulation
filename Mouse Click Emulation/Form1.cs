cf0085e23c35a8dab314d338b5625b8dc16df730
Alina P <grand_duel@rambler.ru>
2020-11-07 15:05:59 +0300

    AP: Timed mouse click

---
+++ Mouse Click Emulation/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Mouse_Click_Emulation;
+
+namespace Mouse_Click_Emulation
+{
+    public partial class Form1 : Form
+    {
+        private int _counter;
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+            this.timer1.Enabled = false;
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.timer1.Enabled = true;
+            if (this.timer1.Enabled)
+            { this.timer1.Start(); }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+                _counter++;
+                this.textBox1.Text = "0:" + (_counter / 60).ToString("00") + ":"  + ( _counter - ((_counter / 60) * 60)).ToString("00");
+                if (_counter % 120 == 0)
+                {
+                    Mouse_Click_Emulation.MouseClickEmulator.MouseClickEvent();
+                }
+       
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.timer1.Stop();
+            this.timer1.Enabled = false;
+        }
+    }
+}
---
+++ Mouse Click Emulation/MouseClickEmulator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Mouse_Click_Emulation
+{
+    partial class MouseClickEmulator
+    {
+
+        [DllImport("user32.dll")]
+        private static extern void mouse_event(int dwFlags, int x, int y, int dwData, int dwExtraInfo);
+
+        private const int MOUSECLICKCONST = 0x00000008;
+        public static void MouseClickEvent()
+        {
+
+            mouse_event
+                (MOUSECLICKCONST,
+                 Cursor.Position.X,
+                 Cursor.Position.Y,
+                 0,
+                 0)
+                ;
+        }
+
+
+    }
+}
